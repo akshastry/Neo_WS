40096e8a14d342bc1e8fddabb37a438b51787dd4
Abhishek Shastry <shastry@terpmail.umd.edu>
2021-10-01 21:17:34 -0400

    vel_feedback_added...might need more tuning

--- scripts/tracking_vel_KF.py
+++ scripts/tracking_vel_KF.py
@@ -16,7 +16,7 @@ from datetime import datetime
 now = datetime.now() # current date and time
 filename = "/home/su/Dropbox/Quadrotor_flight_control/Arduino_playground/LOGS/odom_logs/"
 filename += now.strftime("%Y_%m_%d_%H_%M_%S")
-filename += "data_bank.csv"
+filename += "speed=5__1.csv"
 
 
 mass = 1.46; # mass of quad
@@ -26,17 +26,17 @@ Thrust_sf = 2.3/(mass*g);
 radio_on = 0
 
 
-Kp_x = 1.5#2.5
+Kp_x = 1.0#2.5
 Kd_x = 1.5#0.001
-Ki_x = 0.003
+Ki_x = 0.004
 
 Kp_y = 1.0#2.5
 Kd_y = 1.5#0.001
-Ki_y = 0.003
+Ki_y = 0.005
 
-Kp_z = 7.0#2.0
-Kd_z = 5.0#0.9
-Ki_z = 0.03#0.01
+Kp_z = 5.0#2.0#2.0
+Kd_z = 3.5#1.5#0.9
+Ki_z = 0.01#0.01
 
 Kp_yaw = 5.0
 Ki_yaw = 0.01
@@ -68,7 +68,7 @@ Ki_z_t = 0.01
 
 
 # desired
-X_d = 0.5;
+X_d = 0.65;
 Y_d = 0.0;
 Z_d = -0.75;
 yaw_d 	= 0.0;
@@ -116,7 +116,7 @@ Z_t1 = 0.0;
 
 dX_t = 0.1;
 dY_t = 0.03;
-dZ_t = 0.7; # hieght above the marker to park the vehicle
+dZ_t = 0.5; # hieght above the marker to park the vehicle
 phi_t 	= 0.0;
 theta_t = 0.0;
 psi_t 	= 0.0;
@@ -129,6 +129,9 @@ autonomy_mode = True
 
 # initializing aruco_detect_time
 aruco_detect_time = 0.0
+
+# kalman predict time
+kalman_predict_time = 0.0
 
 # T265_detect time
 pose_received_time = 0.0
@@ -172,6 +175,20 @@ def Kalman_Filter1(x_k, P_k, u_k, z_k, sigma_P2, sigma_u2, sigma_M2, dt1):
 
 	return x_k, P_k
 
+def Kalman_predict(x_k, P_k, u_k, sigma_P2, sigma_u2, dt1):
+	F_KF = np.array([[1.0, dt1], [0.0, 1.0]])
+	F_KF_T = F_KF.T
+
+	Q_KF  = np.array([[1.0/3.0*dt1**3.0, 0.5*dt1**2.0], [0.5*dt1**2.0, 1.0*dt1]])	
+	Qu_KF = np.array([[1.0/4.0*dt1**4.0, 0.5*dt1**3.0], [0.5*dt1**3.0, dt1**2.0]])
+
+	x_k[0] = F_KF[0,0] * x_k[0] + F_KF[0,1] * x_k[1] + 0.5*dt1**2 * u_k
+	x_k[1] = F_KF[1,0] * x_k[0] + F_KF[1,1] * x_k[1] + dt1 * u_k
+
+	P_k  = np.matmul(np.matmul(F_KF, P_k), F_KF_T)
+	P_k = P_k + Qu_KF * sigma_u2 + Q_KF * sigma_P2 # sigma_P2 is continuous time white noise intensity
+
+	return x_k, P_k
 
 def conn_callback(data):
 	global realsense_connected
@@ -225,9 +242,13 @@ VX1 = 0
 VY1 = 0
 VX2 = 0
 VY2 = 0
-y_k = np.array([0.0, 0.0])
-P_k = np.array([[1.0, 0.0],[0.0 ,1.0]])
+y_k  = np.array([0.0, 0.0])
+Py_k = np.array([[1.0, 0.0],[0.0 ,1.0]])
+x_k  = np.array([0.0, 0.0])
+Px_k = np.array([[1.0, 0.0],[0.0 ,1.0]])
+X_t2 = 0
 Y_t2 = 0
+Z_t2 = 0
 def aruco_callback(data):
 	global X_t, Y_t, Z_t, phi_t, theta_t, psi_t
 	global X_t1, Y_t1, Z_t1, X_t0, Y_t0, X_t02, Y_t02
@@ -241,10 +262,12 @@ def aruco_callback(data):
 
 	global fo, filename, file_write_ctr
 	global err_sum_y
-	global y_k, P_k, Y_t2
+	global x_k, y_k, Px_k, Py_k, X_t2, Y_t2, Z_t2, kalman_predict_time
+	global delay_time
 
-	dt1 = rospy.get_time() - aruco_detect_time
-	aruco_detect_time = rospy.get_time()
+	aruco_detect_time 	= rospy.get_time()
+	dt1 = aruco_detect_time - kalman_predict_time
+	kalman_predict_time = aruco_detect_time
 
 
 	#get position
@@ -281,39 +304,40 @@ def aruco_callback(data):
 	# X_t0 = X_t1
 	# Y_t0 = Y_t1
 
-	VX2 = (1-0.25) * VX2 + 0.25 * (X_t2 - X_t02)/dt1
-	VY2 = (1-0.25) * VY2 + 0.25 * (Y_t2 - Y_t02)/dt1
-	X_t02 = X_t2
-	Y_t02 = Y_t2
+	# VX2 = (1-0.25) * VX2 + 0.25 * (X_t2 - X_t02)/dt1
+	# VY2 = (1-0.25) * VY2 + 0.25 * (Y_t2 - Y_t02)/dt1
+	# X_t02 = X_t2
+	# Y_t02 = Y_t2
 
-	y_k, P_k = Kalman_Filter1(y_k, P_k, -aY, Y_t2, 10**(-3.0), 10**(-3.0), 0.1*10**(-4.0), dt1)
+	x_k, Px_k = Kalman_Filter1(x_k, Px_k, -aX, X_t2, 10**(-3.0), 10**(-3.0), 0.03*10**(-4.0), dt1)
+	y_k, Py_k = Kalman_Filter1(y_k, Py_k, -aY, Y_t2, 10**(-3.0), 10**(-3.0), 0.03*10**(-4.0), dt1)
 	# rospy.loginfo("%f, %f\n", y_k[0], Y_t1)
 
 	#get orientation
-	# q0 = data.pose.orientation.w
-	# q1 = data.pose.orientation.x
-	# q2 = data.pose.orientation.y
-	# q3 = data.pose.orientation.z
+	q0 = data.pose.orientation.w
+	q1 = data.pose.orientation.x
+	q2 = data.pose.orientation.y
+	q3 = data.pose.orientation.z
 
-	# cmra2mrkr = R.from_quat([q1, q2, q3, q0])
-	# bdy2cmra  = R.from_euler('ZYX', [np.pi/2.0, 0.0 ,0.0])
-	# mrkr2trgt = R.from_euler('ZYX', [np.pi/2.0, 0.0, np.pi]) # mrkr is aruco marker frame, trgt is same frame but with axes parallel to body axes
+	cmra2mrkr = R.from_quat([q1, q2, q3, q0])
+	bdy2cmra  = R.from_euler('ZYX', [np.pi/2.0, 0.0 ,0.0])
+	mrkr2trgt = R.from_euler('ZYX', [np.pi/2.0, 0.0, np.pi]) # mrkr is aruco marker frame, trgt is same frame but with axes parallel to body axes
 
-	# bdy2trgt = bdy2cmra * cmra2mrkr * mrkr2trgt
-	# psi, theta, phi = bdy2trgt.as_euler('ZYX')
+	bdy2trgt = bdy2cmra * cmra2mrkr * mrkr2trgt
+	psi, theta, phi = bdy2trgt.as_euler('ZYX')
 
-	# psi_t 	= (1 - LP_aruco1) * psi_t 	+ LP_aruco1 * psi
+	psi_t 	= (1 - LP_aruco1) * psi_t 	+ LP_aruco1 * psi
 	# theta_t = (1 - LP_aruco1) * theta_t + LP_aruco1 * theta
 	# phi_t 	= (1 - LP_aruco1) * phi_t 	+ LP_aruco1 * phi
 
 
 
-	# dyaw_d = 0.01 * (psi_t)# - np.pi/2.0)
-	# yaw_d = yaw_d + dyaw_d
+	dyaw_d = 0.01 * (psi_t)# - np.pi/2.0)
+	yaw_d = yaw_d + dyaw_d
 
 
 	# data = "%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f\n"%(rospy.get_time(), X_t1, Y_t1, Z_t1, X_t2, Y_t2, Z_t2, X1, Y1, Z1, X2, Y2, Z2, VX1, VY1, VX2, VY2, pose_received_time, X, Y, Z, VX, VY, VZ, y_k[0], y_k[1])
-	data = "%f,%f,%f,%f,%f\n"%(aruco_detect_time, Y_t2, y_k[0], VY2, y_k[1])
+	data = "%f,%f,%f,%f,%f\n"%(aruco_detect_time, x_k[0], y_k[0], x_k[1], y_k[1])
 	
 	fo.write(data)
 	file_write_ctr = file_write_ctr  + 1
@@ -328,6 +352,7 @@ def aruco_callback(data):
 
 	if(autonomy_mode):
 		print('entering aruco_mode')
+		delay_time = rospy.get_time()
 		# err_sum_y = 0
 	autonomy_mode = False
 
@@ -365,22 +390,36 @@ def autonomy_control():
 	global yaw
 	global aruco_detect_time, autonomy_mode
 	global X_t, Y_t, Z_t, psi_t
-	global X_t1, Y_t1, Z_t1
+	global X_t2, Y_t2, Z_t2
 	global dX_t, dY_t, dZ_t
 
 	global err_sum_xint, err_sum_yint, Ki_xint, Ki_yint
 
 	global err_X_prev, err_Y_prev
 
+	global VX2, VY2
+
+	global x_k, Px_k, aX, y_k, Py_k, aY, kalman_predict_time
+
 	if (autonomy_mode):
 		err_X = X_d - X
 		err_Y = Y_d - Y
+		err_Z = Z_d - Z
 	else:
-		err_X = X_t1
-		err_Y = Y_t1
+		curr_time = rospy.get_time()
+		dt1 	  = curr_time - kalman_predict_time
+		kalman_predict_time = curr_time
+
+		x_k, Px_k = Kalman_predict(x_k, Px_k, -aX, 10**(-3.0), 10**(-3.0), dt1)
+		y_k, Py_k = Kalman_predict(y_k, Py_k, -aY, 10**(-3.0), 10**(-3.0), dt1)
+
+		err_X = x_k[0]
+		err_Y = y_k[0]
+		err_Z = Z_d - Z
+		# err_Z = Z_t2
 	
 
-	err_Z = Z_d - Z
+	# err_Z = Z_d - Z
 	err_yaw = yaw_d - yaw
 
 	err_sum_x 	= constrain(err_sum_x + err_X, -1.0/Ki_x, 1.0/Ki_x)
@@ -390,19 +429,24 @@ def autonomy_control():
 
 
 	# xdd = Kp_x * err_X + 120 * Kd_x * (err_X - err_X_prev) + Ki_x * err_sum_x
-	if (autonomy_mode):
+	if (autonomy_mode == True or rospy.get_time() - delay_time < 1.0):
 		ydd = Kp_y * err_Y + 2*Kd_y * (0.0 - VY) + Ki_y * err_sum_y
 		xdd = Kp_x * err_X + 2*Kd_x * (0.0 - VX) + Ki_x * err_sum_x
 	else:
 		# ydd = 1.0 * err_Y + 80 * 2.5 * (err_Y - err_Y_prev) + 0.0005 * err_sum_y
 		# ydd = 3.0 * err_Y + 500 * 2.5 * (err_Y - err_Y_prev) + 0.0001 * err_sum_y + Kd_y * (0.0 - VY) 
-		xdd = Kp_x * err_X + Kd_x * (0.0 - VX) + Ki_x * err_sum_x + 120 * Kd_x * (err_X - err_X_prev)
-		ydd = Kp_y * err_Y + Kd_y * (0.0 - VY) + Ki_y * err_sum_y + 80 * Kd_y * (err_Y - err_Y_prev)
+		# xdd = Kp_x * err_X + Kd_x * (0.0 - VX) + Ki_x * err_sum_x + 120 * Kd_x * (err_X - err_X_prev)#+ 3 * Kd_x * VX2
+		# xdd = Kp_x * err_X + 1.6 * Kd_x * (0.0 - VX) + Ki_x * err_sum_x +  1.5 * Kd_x * VX2
+		xdd = Kp_x * err_X + 1.6 * Kd_x * (0.0 - VX) + Ki_x * err_sum_x + 1.7 * Kd_x * x_k[1]
+		ydd = Kp_y * err_Y + 1.3 * Kd_y * (0.0 - VY) + Ki_y * err_sum_y + 1.8 * Kd_y * y_k[1]
+
+		# ydd = Kp_y * err_Y + Kd_y * (0.0 - VY) + Ki_y * err_sum_y + 80 * Kd_y * (err_Y - err_Y_prev)
 		# print(err_sum_y)
 	
 	# xdd = Kp_x * err_X + Kd_x * (0.0 - VX) + Ki_x * err_sum_x
-	# ydd = Kp_y * err_Y + Kd_y * (0.0 - VY) + Ki_y * err_sum_y
+	# ydd = Kp_y * err_Y + 2*Kd_y * (0.0 - VY) + Ki_y * err_sum_y
 	zdd = Kp_z * err_Z + Kd_z * (0.0 - VZ) + Ki_z * err_sum_z
+	# print(err_Z, zdd)
 
 	r_d	= Kp_yaw * err_yaw + Ki_yaw * err_sum_yaw
 
@@ -493,7 +537,7 @@ def main():
 
 			ctrl.linear.x    = radio_on
 			ctrl.linear.y 	= 0.0
-			ctrl.linear.z 	= constrain(T_d * Thrust_sf, 0.0, 2.75)
+			ctrl.linear.z 	= constrain(T_d * Thrust_sf, 0.0, 2.6)
 			ctrl.angular.x 	= constrain(phi_d * 180.0/np.pi, -10.0, 10.0) 
 			ctrl.angular.y 	= constrain(theta_d * 180.0/np.pi, -10.0, 10.0)
 			ctrl.angular.z 	= constrain(r_d * 180.0/np.pi, -30, 30)
